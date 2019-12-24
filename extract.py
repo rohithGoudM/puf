bd95aaf997fc2ed52fd1d45718b6224205ed7d9e
Rohith Goud <mrohithg@gmail.com>
2019-12-24 18:58:13 +0530

    cleared a bug in feature extraction

--- extract.py
+++ extract.py
@@ -20,34 +20,23 @@ def custom_phi(x):
 rows = len(df)
 columns = len(df[0])
 
-print(rows, columns)
-print(df)
-
-
 for r in range(rows):
-	# print(df[r])
 	x = 0
 	flag = 0
 	for i in range(columns):
 		if(df[r][i]==0):
 			x=x+1
-	# print(r, x, df[r][i], pf[r][i])
 	for i in range(columns):
 		if(df[r][i]==0):
 			flag=1
-		# pf[r][i] = custom_phi(x)
+		pf[r][i] = custom_phi(x)
 		if(flag==1):
 			flag=0
 			x=x-1
-	# print(r, x, df[r][i], pf[r][i])
 
-# pd.DataFrame(df).to_csv("extracted.csv")
-# a = numpy.asarray([ [1,2,3], [4,5,6], [7,8,9] ])
 a = pf
 np.savetxt("data/features.csv", a, delimiter=",")
 
-#Your statements here
 
 stop = timeit.default_timer()
-
 print('Time: ', stop - start) 
